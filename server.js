4d07676fe48fbccfc1d6ac501dd64496e54eac33
manya <93120998+MANYA1011@users.noreply.github.com>
2023-12-29 15:22:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path'
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
 
+const __filename=fileURLToPath(import.meta.url);
+cont __dirname=path.dirname(__filename);
 //rest object
 const app = express();
 
